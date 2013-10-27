0f19fa7837188e1563f8add5ffea16e6b190a75e
Rahul-Singhal <singhalrahul43@gmail.com>
2013-10-27 22:58:32 +0530

    try

--- profile.php
+++ profile.php
@@ -17,7 +17,6 @@
   <head>
     <title>Mini-Facebook</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 	<link href="css/datepicker.css" rel="stylesheet">
 	<style type="text/css">
